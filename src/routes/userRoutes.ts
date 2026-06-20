6d94c795e69697b48d7ed47b59bd32e4a4224131
tya01-ux <dindafitria283@gmail.com>
2026-06-20 12:13:42 +0700

    fix: precise casing for controller imports to satisfy linux build

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getProfile } from "../controllers/userController.js";  
+import { getProfile } from "../controllers/userController.js";
 import { authenticate } from "../middlewares/authMiddleware.js"; 
 
 const router = Router();
