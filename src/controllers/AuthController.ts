a044072a7a3bb2dc92adea60e436eea871d870a8
tya01-ux <dindafitria283@gmail.com>
2026-07-07 00:39:02 +0700

    add authController

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { registerUser, loginUser } from "../services/AuthService.js";
+import { registerUser, loginUser } from "../services/authService.js";
 
 export const register = async (req: Request, res: Response) => {
     try {
