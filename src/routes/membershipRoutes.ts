293438fcdb52b4915b870f6b057d5a8ddc72d01f
tya01-ux <dindafitria283@gmail.com>
2026-07-08 14:30:11 +0700

    member

--- src/routes/membershipRoutes.ts
+++ src/routes/membershipRoutes.ts
@@ -6,13 +6,14 @@ import {
   updateMembership,
   deleteMembership,
 } from "../controllers/membershipController.js";
+import { authenticate, checkAdmin } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
 router.get("/", getMemberships);
 router.get("/:id", getMembership);
-router.post("/", addMembership);
-router.put("/:id", updateMembership);
-router.delete("/:id", deleteMembership);
+router.post("/", authenticate, checkAdmin, addMembership);
+router.put("/:id", authenticate, checkAdmin, updateMembership);
+router.delete("/:id", authenticate, checkAdmin, deleteMembership);
 
 export default router;
