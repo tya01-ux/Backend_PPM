abfeb12632a6e932767fcfd8118178cd0d31e00f
tya01-ux <dindafitria283@gmail.com>
2026-07-10 20:20:15 +0700

    schema notif

---
+++ prisma/migrations/20260710131952_add_notificationschema/migration.sql
@@ -0,0 +1,18 @@
+-- CreateTable
+CREATE TABLE `Notification` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `userId` INTEGER NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `message` VARCHAR(191) NOT NULL,
+    `type` ENUM('booking_new', 'booking_confirmed', 'booking_cancelled', 'booking_completed', 'membership_new', 'membership_active', 'membership_rejected', 'schedule_new') NOT NULL,
+    `isRead` BOOLEAN NOT NULL DEFAULT false,
+    `link` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    INDEX `Notification_userId_idx`(`userId`),
+    INDEX `Notification_userId_isRead_idx`(`userId`, `isRead`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Notification` ADD CONSTRAINT `Notification_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
